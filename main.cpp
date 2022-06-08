ae816f4c858b78ae0c2aff4375877bd502f95dc4
Uchendu Nwachukwu <nwachuch@students.zhaw.ch>
2022-06-08 08:55:47 -0700

    make example more clear with random data access pattern

--- main.cpp
+++ main.cpp
@@ -23,17 +23,31 @@ int main(int argc, char* argv[]){
 
   const int DSIZE = 64*1048576;
 
-  // Initialize vectors
-  vecmem::vector<float> a(DSIZE);
-  vecmem::vector<float> b(DSIZE);
-  std::vector<float> c(DSIZE);
+  // populate vectors
+  vecmem::vector<float> a_initialized(DSIZE);
+  vecmem::vector<float> b_initialized(DSIZE);
   for (int i = 0; i < DSIZE; i++){
-      a.at(i) = rand()/(float)RAND_MAX;
-      b.at(i) = rand()/(float)RAND_MAX;
+      a_initialized.at(i) = rand()/(float)RAND_MAX;
+      b_initialized.at(i) = rand()/(float)RAND_MAX;
   }
 
+  // initialize const and std vectors
+  const vecmem::vector<float> a = a_initialized;
+  const vecmem::vector<float> b = b_initialized;
+  std::vector<float> c(DSIZE);
+
+  // get data pointers
+  const float* a_p = a.data();
+  const float* b_p = b.data();
+  float* c_p = c.data();
+
   // execute 
-  std::transform(std::execution::par, a.begin(), a.end(), b.begin(), c.begin(), [](float x, float y) -> float {return x+y;});
+  std::for_each_n(std::execution::par, counting_iterator(0), DSIZE, [=](unsigned int i){
+    c_p[i] = a_p[i] + b_p[i];
+  });
+
+  // alternative execution call
+  // std::transform(std::execution::par, a.begin(), a.end(), b.begin(), c.begin(), [](float x, float y) -> float {return x+y;});
 
   // print out some results to verify calculation
   std::cout << "-----------\n";
